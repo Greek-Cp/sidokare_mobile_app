f7475383db39c1132d52db92279e7d98d426c1d2
Erere <82995911+Greek-Cp@users.noreply.github.com>
2023-03-29 12:29:46 +0700

    Pembaruan dari gw
    ini daf

---
+++ lib/pages/item_nav/page_berita.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PageBerita extends StatefulWidget {
+  @override
+  State<PageBerita> createState() => _PageBeritaState();
+}
+
+class _PageBeritaState extends State<PageBerita> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
